6b7eb3eb42ab9ba9687a62e67dfa5a475d51b147
Felipe López <jualopezra@unal.edu.co>
2022-08-29 22:47:25 -0500

    1

---
+++ src/main/java/taller3/televisores/Control.java
@@ -0,0 +1,36 @@
+package taller3.televisores;
+
+public class Control {
+	TV tv;
+	public void enlazar(TV tv) {
+		this.tv=tv;
+		tv.setControl(this);
+	}
+	public void setTV(TV tv) {
+		this.tv=tv;
+	}
+	public TV getTV() {
+		return this.tv;
+	}
+	public void turnOn() {
+		tv.turnOn();
+	}
+	public void turnOff() {
+		tv.turnOff();
+	}
+	public void volumenUp() {
+		tv.volumennUp();
+	}
+	public void volumenDown() {
+		tv.volumenDown();
+	}
+	public void canalUp() {
+		tv.canalUp();
+	}
+	public void canalDown() {
+		tv.canalDown();
+	}
+	public void setCanal(int canal) {
+		tv.setCanal(canal);
+	}
+}
---
+++ src/main/java/taller3/televisores/TV.java
@@ -0,0 +1,91 @@
+package taller3.televisores;
+
+public class TV {
+	Marca marca;
+	int canal = 1;
+	int precio = 500;
+	boolean estado;
+	int volumen = 1;
+	Control control;
+	static int numTV= 0;
+	public TV(Marca marca, boolean estado) {
+		this.marca= marca;
+		this.estado= estado;
+		numTV++;
+	}
+	public void setCanal(int canal) {
+		if(this.estado==true) {
+			if(canal>=1 && canal<=120) {
+				this.canal=canal;
+			}
+		}
+	}
+	public int getCanal() {
+		return this.canal;
+	}
+	public void setPrecio(int precio) {
+		this.precio=precio;
+	}
+	public int getPrecio() {
+		return this.precio;
+	}
+	public void setVolumen(int volumen) {
+		this.volumen=volumen;
+	}
+	public int getVolumen() {
+		return this.volumen;
+	}
+	public void setMarca(Marca marca) {
+		this.marca=marca;
+	}
+	public Marca getMarca() {
+		return this.marca;
+	}
+	public void setControl(Control control) {
+		this.control=control;
+	}
+	public Control getControl() {
+		return this.control;
+	}
+	public static int getNumTV() {
+		return TV.numTV;
+	}
+	public void turnOn() {
+		this.estado= true;
+	}
+	public void turnOff() {
+		this.estado=false;
+	}
+	public boolean getEstado() {
+		return this.estado;
+	}
+	public void canalUp() {
+		if(this.estado==true) {
+			if(this.canal>=1 && this.canal<= 120) {
+				this.canal++;
+			}
+		}
+	}
+	public void canalDown() {
+		if(this.estado==true) {
+			if(this.canal>=1 && this.canal<= 120) {
+				this.canal--;
+			}
+		}
+	}
+	public void volumennUp() {
+		if(this.estado==true) {
+			if(this.volumen>=0 && this.volumen<= 7) {
+				this.volumen++;
+			}
+		}
+	}
+	public void volumenDown() {
+		if(this.estado==true) {
+			if(this.volumen>=0 && this.volumen<= 7) {
+				this.volumen--;
+			}
+		}
+	}
+	
+}
