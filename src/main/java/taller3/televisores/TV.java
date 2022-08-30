b319bafba718842fd08d09cb04d804d7feeff931
Felipe López <jualopezra@unal.edu.co>
2022-08-29 23:39:32 -0500

    Update TV.java

--- src/main/java/taller3/televisores/TV.java
+++ src/main/java/taller3/televisores/TV.java
@@ -63,12 +63,12 @@ public class TV {
 		return this.estado;
 	}
 	public void canalUp() {
-		if(this.estado==true && this.canal>=0&& this.canal< 120) {
+		if(this.estado==true && 0<=this.canal && this.canal< 120) {
 			this.canal++;
 			}
 		}
 	public void canalDown() {
-		if(this.estado==true && this.canal>0&& this.canal<= 120) {
+		if(this.estado==true && 0<this.canal&& this.canal<= 120) {
 			this.canal--;			}
 		}
 	public void volumenUp() {
